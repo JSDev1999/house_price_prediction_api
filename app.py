7639a66a9143322b4cb10d08ad7cbf007d1457db
JSDev1999 <rameshlearningadvance@gmail.com>
2023-06-12 16:01:08 +0530

    updated

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-import util
-
+from util import load_saved_artifacts, get_location_names, get_estimated_price
 app = Flask(__name__)
 CORS(app)
  
-util.load_saved_artifacts()
+load_saved_artifacts()
 
 @app.route('/', methods=['GET'])
 def Home():
     return " Hello from api!! ,Have fun"
 
 @app.route('/get_location_names', methods=['GET'])
-def get_location_names():
-    locations = util.get_location_names()
+def get_location_names_fun():
+    locations = get_location_names()
     print(locations)
     response = jsonify({
         'locations': locations
@@ -31,7 +30,7 @@ def predict_home_price():
     bath = int(request.json['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        'estimated_price': get_estimated_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
